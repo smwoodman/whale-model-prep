0d9a32ba91937a09811b74a4e82c957c53f60eaa
smwoodman <sam.woodman@noaa.gov>
2018-11-16 15:47:47 -0800

    Finished ETOPO extraction with extra stuff

--- ETOPO_Extractions_WEAR.R
+++ ETOPO_Extractions_WEAR.R
@@ -8,12 +8,16 @@
 rm(list = ls())
 
 #-------------------------------SET UP FUNCTIONS---------------------------------------
-# If needed install and load ncdf4 and dplyr packages
-list.of.packages <- c("ncdf4", "dplyr", "purrr")
-
+# Install (if needed) and then load packages
+list.of.packages <- c("ncdf4", "dplyr", "purrr", "sf")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if(length(new.packages)) install.packages(new.packages) #install packages
-lapply(new.packages, require, character.only = TRUE)    #load packages
+rm(list.of.packages, new.packages)
+
+library(ncdf4)
+library(dplyr)
+library(purrr)
+library(sf)
 
 
 #-------------------------------END OF FUNCTIONS---------------------------------------
@@ -45,7 +49,7 @@ out.data <- in.data
 
 
 #------------------------------------------------------------------------------
-# Now get depth
+### Depth prep work
 nc.file <- paste0(etopo.path,'etopo180_N10-60_W150-100.nc')
 nc.data <- nc_open(nc.file)
 ETOPO.lat   <- ncvar_get(nc.data, 'latitude')
@@ -61,49 +65,104 @@ ETOPO.ncols <- length(ETOPO.lat)
 # colnames(depth) <- ETOPO.lat
 # write.csv(depth, paste0(etopo.path, 'ETOPO1_CCS_bathy.csv'))
 
+
 # for() loop moved to bottom of file
-z <- 0
+# ~5.8 seconds without special stuff
+# ~11.5 seconds with special stuff
+
+
+#------------------------------------------------------------------------------
+### Create list-column with both depth and SD(depth) values
+# Sam note: nc file is indexed in order of (X, Y, Z, T) and ncvar_get() output
+#   reflects that (i.e. rows correspond to lon and cols to lat of pred.data)
 out.data$depth_lc <- apply(cbind(lon, lat), 1, function(i) {
   if (anyNA(i)) {
     NA
     
   } else {
+    # Get ETOPO row and column number of ETOPO point closest to grid centroid
+    # This assumes ETOPO is finer-scale than the grid and spans entire grid
     r.lon <- which.min(abs(ETOPO.lon - i[1]))
     c.lat <- which.min(abs(ETOPO.lat - i[2]))
     
-    # nrows and ncols are used if we are at the edge of the data grid
+    # nrows and ncols are used if we are at the edge of the ETOPO grid
     row1    <- max(r.lon - 1, 1)
     numrows <- min(r.lon + 1, ETOPO.nrows) - row1 + 1  
     col1    <- max(c.lat - 1, 1)                        
     numcols <- min(c.lat + 1, ETOPO.ncols) - col1 + 1
     
-    # newlon <- ETOPO.lon[row1:(row1 + numrows - 1)]
-    # newlat <- ETOPO.lat[col1:(col1 + numcols - 1)]
-    
+    # Get depth value of closest ETOPO point and 9 surrounding ETOPO values
     pred.data <- ncvar_get(
       nc.data, 'altitude', start = c(row1, col1),
       count = c(numrows, numcols), verbose = FALSE
-    ) 
+    )
+    pred.cent <- pred.data[1 + (r.lon - row1), 1 + (c.lat - col1)][1]
     
-    if (pred.data[1 + (r.lon - row1), 1 + (c.lat - col1)][1] > 0) {
-      z <<- z + any(pred.data >= 0)
+    # If the centroid is closest to a land ETOPO point (>=0) but 
+    #   at least one of the 9 surrounding ETOPO points are ocean (<0), 
+    #   then get the value of the closest ocean ETOPO point
+    #   that is still within the grid cell
+    if (pred.cent >= 0 && any(pred.data < 0)) {
+      ## Create sf objects of ETOPO points, grid cell centroid, and grid cell
+      # ETOPO points
+      depth.coords <- expand.grid(
+        as.numeric(ETOPO.lon[row1:(row1 + numrows - 1)]), 
+        as.numeric(ETOPO.lat[col1:(col1 + numcols - 1)])
+      )
+      depth.all <- as.vector(pred.data)
+      
+      depth.sf <- depth.coords %>% 
+        mutate(depth = depth.all) %>%  #as.vector() combines by column
+        st_as_sf(coords = c(1, 2), crs = 4326)
+      
+      # Grid cell centroid
+      cent.sfc <- st_sfc(st_point(i), crs = 4326)
+      
+      # Grid cell (polygon)
+      j <- 0.0225 # Half of grid cell length/width
+      poly.sfc <- st_sfc(st_polygon(list(matrix(
+        c(i[1] + j, i[1] - j, i[1] - j, i[1] + j, i[1] + j,
+          i[2] + j, i[2] + j, i[2] - j, i[2] - j, i[2] + j),
+        ncol = 2
+      ))), crs = 4326); rm(j)
+      
+      ## Determine which of the points that meet the depth (negative) and 
+      ##   polygon (within grid cell) requirements
+      poly.depth.int <- suppressMessages(st_intersects(poly.sfc, depth.sf)[[1]])
+      depth.which <- which(
+        (1:(numcols * numrows) %in% poly.depth.int) & (depth.all < 0)
+      )
+
+      if (length(depth.which) > 0) {
+        ## Which of those points is closest to the centroid
+        cent.depth.dist <- as.numeric(st_distance(cent.sfc, depth.sf))
+        names(cent.depth.dist) <- 1:length(depth.all)
+        
+        cent.depth.dist2 <- cent.depth.dist[depth.which]
+        cent.depth.min.name <- as.numeric(
+          names(cent.depth.dist2)[which.min(cent.depth.dist2)]
+        )
+        
+        pred.cent <- depth.all[cent.depth.min.name]
+      }
     }
     
-    list(
-      pred.data[1 + (r.lon - row1), 1 + (c.lat - col1)][1],
-      sd(pred.data[pred.data < 0])
-    )
+    list(pred.cent, sd(pred.data[pred.data < 0]))
   }
 })
+
+
+#------------------------------------------------------------------------------
+# Extract depth and SD(depth) from list-column and save to their own columns
 out.data <- out.data %>% 
-  mutate(depth = purrr::map_dbl(depth_lc, function(i) i[[1]]), 
-         depth_sd = purrr::map_dbl(depth_lc, function(i) i[[2]])) %>% 
+  mutate(depth = purrr::map_dbl(depth_lc, function(j) j[[1]]), 
+         depth_sd = purrr::map_dbl(depth_lc, function(j) j[[2]])) %>% 
   select(lat, lon180, lon360, area_km, depth, depth_sd)
 
 nc_close(nc.data)
 
 
-###############################################################################
+#------------------------------------------------------------------------------
 # Change depth and depth_sd for positive depth values (i.e. land) to NA
 # (this is only needed for grids which have land)
 
@@ -113,6 +172,12 @@ out.data$depth[land] <- NA
 out.data$depth_sd[land] <- NA
 
 write.table(out.data, outfile, sep = "," , col.names = TRUE, row.names = FALSE)
+
+
+# x <- read.csv("../whale-model-prep_data/Outputs/Grid_Non-rectangle_3km_bathy.csv")
+# y <- read.csv("../whale-model-prep_data/Outputs/Grid_Non-rectangle_3km_bathy_noextra.csv")
+# sum(is.na(x$depth)) #10954
+# sum(is.na(y$depth)) #11344
 
 ###############################################################################
 ###############################################################################
