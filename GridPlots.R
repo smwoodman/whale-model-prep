b91abf291c7470f8b06a23992e265c2abe7fc09d
smwoodman <sam.woodman@noaa.gov>
2019-04-12 14:06:34 -0700

    Adding original GridPlots - code for K & E to automate plotting model outputs in R

---
+++ GridPlots.R
@@ -0,0 +1,65 @@
+##################################### GridPlots.r ##############################################
+#
+#  This code was developed by Sam Woodman to facilitate plotting of cetacean density-model 
+#  prediction grids.  
+#
+#   Written 03/2019  (Originally named "Plotting_for_Karin.r")
+#
+#   03/20/2019:  Renamedto GridPlots.r and edited by KAF for WEAR (Whale entanglement risk 
+#   assessment) project; also added code to print individual plots to a specified PNG file
+#
+##################################################################################################
+# Clear workspace
+
+
+ library(maps)
+ library(sf)
+ library(maptools)
+
+# NOTE: function assumes coordinates are WGS 84 geographic coordinates
+
+# Sam testing
+# density.df <- read.csv("C:/Ensemble Tool/eSDM-data-local/eSDM_data_manuscript/Predictions_Becker_et_al_2016.csv")
+# plot.dens.func(density.df, c(1,2,3), plot.title = "Becker et al", legend.pos = 4, xlim = c(-125, -120), ylim = c(35, 40), cex = 1, pch = 18)
+
+
+
+# Plotting function
+plot.dens.func <- function(plot.file, density.df, col.names, map.base = NULL, pch = 19, cex = 0.5, 
+                           plot.title = "Plot", xlim = NULL, ylim = NULL, legend.pos = 4) {
+  ### Inputs
+  # density.df: Data frame with at least three columns (longitude, latitude, prediction value)
+  # col.names: Vector of three strings (names of columns) or numbers (indices of columns).
+  #   Order of names or indicies must be longitude, latitude, and density values
+  # pch: Point type
+  # plot.title: Plot title, as string
+  # xlim: x axis limits, e.g. c(-135, -115). Use range [-180, 180]
+  # ylim: y axis limits, e.g. c(30, 50)
+  # legend.pos: Legend position: 1 is bottom, 2 is left, 3 is top, and 4 is right of plot
+
+  
+  if (is.null(map.base)) map.base <- st_as_sfc(maps::map('world', plot = FALSE, fill = TRUE))
+  # ^ Can be generated outide and passed to function for efficiency
+  
+  stopifnot(is.data.frame(density.df), length(col.names) == 3)
+  
+  
+  
+  # Convert points to sf object so it will be 'spatially' plotted
+  density.sf <- st_as_sf(density.df[, col.names], coords = c(col.names[1:2]), 
+                         crs = 4326, agr = "constant")
+  # If longitude points were 0-360, this converts everything to -180 to 180 range so map can be plotted
+  density.sf <- st_wrap_dateline(density.sf,options=c("WRAPDATELINE=NO","DATELINEOFFSET=0"))
+
+  png(paste0(plot.file,".png"), width = 8, height = 6, units = 'in', res = 450)
+  
+  plot(density.sf[1], main = plot.title, axes = TRUE, xlim = xlim, ylim = ylim, 
+       pch = pch, cex = cex, 
+       key.length = 1, key.pos = legend.pos, reset = FALSE)
+  plot(map.base, add = TRUE, col = "tan")
+
+  dev.off()
+  
+  }
+
+
