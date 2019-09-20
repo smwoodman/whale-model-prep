fc00dc259cab5f49b9b43773fae4e410d216b15b
Sam Woodman <swoodman213@gmail.com>
2019-09-19 18:25:35 -0700

    Removed Exp_* files

--- Exp_rename_ccsra.R
+++
@@ -1,8 +0,0 @@
-# Rename CCSRA Excel files extracted by JVR
-
-files.path <- "../whale-model-prep_data/Grid/Grid_CCSRA_JVR"
-files.all <- list.files(files.path)
-
-for(i in files.all) {
-  file.rename(paste0(files.path, "/", i), paste0(files.path, "/", substr(i, 11, nchar(i))))
-}; rm(i)
