a15887276d0886dec20f8b14ca7b37ad5153405b
Sam Woodman <swoodman213@gmail.com>
2019-09-27 17:08:16 -0700

    Add whalepreds_evaluate and demo file; function used to calculate AUC and TSS and plot validation data with applicable predictions

---
+++ Whalepreds_evaluate.R
@@ -0,0 +1,260 @@
+### Function for caculating evaluation metrics for whale predictions 
+###   aggregated at a variety of time scales
+
+
+###############################################################################
+whalepreds_evaluate <- function(
+  x, y, x.cols = NULL, x.col.idx, y.cols, grid.rad = 0.027/2, 
+  csv.filename = NULL, plot.path = NULL, 
+  plot.xlim = c(-127, -116.5), plot.ylim = c(31.5, 49), 
+  col.breaks = c(0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.09), col.pal = NULL)
+{
+  ### Inputs
+  # x: data.frame; Aggregated whale predictions
+  # y: data.frame; Validation data
+  # x.cols: character or numeric; vector of length >=3 containing, 
+  #   in order, column names or indices of lon, lat, and whale preds. 
+  #   Defaults to 'c(3, 2, 6:ncol(x))' based on KAF model outputs
+  # x.col.idx: numeric; indices of characters in column names of x that
+  #   specify the date; must be either of length 8 or 10 
+  # y.cols: character or numeric; vector with length of 4 containing, 
+  #   in order, column names or indices of lon, lat, date, and validation data
+  # grid.rad: numeric; radius (half of length of one side) of grid cells
+  #   Default is for ~3km grid: 0.027 / 2
+  # csv.filename: character; path and filename for csv file to which to save
+  #   metrics info; if NULL, csv not saved
+  
+  # plot.path: character; path to folder in which to save plots of preds and 
+  #   validation data; if NULL, plots not made
+  # plot.xlim: numeric of length 2; x axis plot limits, use range [-180, 180]
+  # plot.ylim: numeric of length 2; y axis plot limits, use range [-90, 90]
+  # col.breaks: numeric; length one more than col.pal; break points for 
+  #   color bins
+  # col.pal: numeric; length one less than col.breaks; colors to be used 
+  #   for color bins (from smallest to largest); 
+  #   if NULL, palette will be generated using RColorBrewer 'YlGnBu'
+  
+  
+  ### Output
+  # Data frame with columns for date interval start, AUC vals, and TSS vals
+  
+  #----------------------------------------------------------------------------
+  # Input checks and (some) processing
+  
+  stopifnot(
+    require(dplyr), 
+    require(eSDM), 
+    require(lubridate), 
+    require(purrr), 
+    require(RColorBrewer), 
+    require(readr), 
+    require(sf)
+  )
+  
+  ### Format and length checks
+  if (is.null(x.cols)) x.cols <- c(3, 2, 6:ncol(x))
+  if (is.null(col.pal)) 
+    col.pal <- rev(RColorBrewer::brewer.pal(length(col.breaks) - 1, "YlGnBu"))
+  
+  stopifnot(
+    inherits(x, "data.frame"), 
+    inherits(y, "data.frame"), 
+    inherits(x.cols, c("character", "integer", "numeric")), 
+    length(x.cols) >= 3, 
+    inherits(x.col.idx, c("integer", "numeric")),
+    inherits(y.cols, c("character", "integer", "numeric")), 
+    length(y.cols) == 4, 
+    inherits(plot.xlim, "numeric"), 
+    inherits(plot.ylim, "numeric"), 
+    inherits(col.breaks, "numeric"), 
+    inherits(col.pal, "character"), 
+    length(col.breaks) == (length(col.pal) + 1)
+  )
+  
+  names(plot.xlim) <- c("xmin", "xmax") #for st_bbox() calls below
+  names(plot.ylim) <- c("ymin", "ymax") #for st_bbox() calls below
+  
+  
+  #----------------------------------------------------------------------------
+  # Process whale preds and get dates from column names
+  
+  ### Process preds and create geometry for x
+  x.preds <- x %>%
+    select(!!x.cols[-c(1:2)]) %>%
+    set_names(substr(names(.), min(x.col.idx), max(x.col.idx)))
+  
+  x.geom <- x %>%
+    select(!!x.cols[1:2]) %>%
+    pts2poly_centroids(grid.rad, crs = 4326)
+  
+  
+  ### Get dates from column names and create intervals
+  # What are dates separated by?
+  tmp <- if (all(grepl("[.]", names(x.preds)))) {
+    "."
+  } else if (all(grepl("_", names(x.preds)))) {
+    "_"
+  } else if (all(grepl("-", names(x.preds)))) {
+    "-"
+  } else {
+    stop("The dates in the column names must all be separated by '.', '_', or '-'")
+  }
+  
+  # Generate dates from column names (are dates 8 digit or 10 digit?)
+  cols.dates <- switch(
+    as.character(length(x.col.idx)), 
+    "8" = as.Date(names(x.preds), format = paste0("%y", tmp, "%m", tmp, "%d")), 
+    "10" = as.Date(names(x.preds), format = paste0("%Y", tmp, "%m", tmp, "%d")), 
+    stop("x.col.idx must be of length 8 (e.g. \"05_01_01\" or \"05.01.01\") ", 
+         "or 10 (e.g. \"2005_01_01\" or \"2005.01.01\")")
+  ); rm(tmp)
+  
+  # Create intervals captured by each column
+  d <- unique(diff(cols.dates))
+  dates.diff <- ifelse(length(d == 1), d, NA)
+  d.last <- ifelse(
+    is.na(dates.diff),  tail(cols.dates, 1) + months(1), 
+    tail(cols.dates, 1) + days(dates.diff)
+  )
+  cols.intervals.ends <- c(
+    tail(cols.dates, -1), 
+    ifelse(
+      is.na(dates.diff),  tail(cols.dates, 1) + months(1), #if interval is monthly
+      tail(cols.dates, 1) + days(dates.diff)               #if interval is # of days
+    )
+  )
+  
+  cols.intervals <- interval(cols.dates, cols.intervals.ends - days(1))
+  rm(d, dates.diff, d.last, cols.intervals.ends)
+  
+  
+  #----------------------------------------------------------------------------
+  ### Process validation data
+  y.data <- y %>% 
+    select(lon = !!y.cols[1], lat = !!y.cols[2], 
+           dt = !!y.cols[3], valid_data = !!y.cols[4])
+  
+  stopifnot(
+    inherits(y.data$lon, c("integer", "numeric")), 
+    inherits(y.data$lat, c("integer", "numeric")), 
+    inherits(y.data$dt, "Date"), 
+    inherits(y.data$valid_data, c("integer", "numeric"))
+  )
+  
+  
+  #----------------------------------------------------------------------------
+  ### Determine which validation data are in intervals, and which 
+  ###   intervals contain validation data
+  valid.which <- vapply(y.data$dt, function(i, j) {
+    i.which <- which(i %within% j)
+    if (length(i.which) > 1)
+      stop("Error allocating validation data at date ", i, 
+           " to a single prediction interval")
+    if(length(i.which) == 0) NA else i.which
+  }, 1, j = cols.intervals)
+  
+  cols.intervals.which <- sort(unique(unlist(valid.which)))
+  
+  
+  #----------------------------------------------------------------------------
+  ### Filter data, and create sf objects for preds and validation data
+  x.sf <- x.preds %>% 
+    select(cols.intervals.which) %>% 
+    st_sf(geometry = x.geom, agr = "constant")
+  
+  y.sf <- y.data %>% 
+    mutate(valid_data_pa = ifelse(valid_data >= 1, 1, 0), 
+           preds_col = names(x.preds)[valid.which]) %>% 
+    filter(!is.na(preds_col)) %>% 
+    st_as_sf(coords = c("lon", "lat"), crs = 4326, agr = "constant")
+  
+  
+  #----------------------------------------------------------------------------
+  # Calculate metrics for dates with both preds and validation data
+  
+  ### Dates with both preds and validation data
+  dates.both <- sort(unique(y.sf$preds_col))
+  if (length(dates.both) == 0) 
+    stop("No dates have both predictions and validation data - \nhave ", 
+         "predictions been aggregated correctly, and is 'x.col.idx' correct?")
+  
+  print(paste("Number of dates:", length(dates.both)))
+  
+  ### Prep for plotting, if necessary
+  if (!is.null(plot.path)) {
+    hw <- diff(plot.ylim) / diff(plot.xlim)
+    check.sfc <- st_as_sfc(st_bbox(c(plot.xlim, plot.ylim), crs = 4326))
+    crop.bbox  <- st_bbox(c(plot.xlim , plot.ylim) + c(-1, 1), crs = 4326)
+    
+    map.base <- st_as_sfc(maps::map('world', plot = FALSE, fill = TRUE))
+    map.base <- suppressMessages(st_crop(map.base, crop.bbox))
+    
+    x.sf.crop <- suppressMessages(st_crop(x.sf, crop.bbox))
+    
+    tmp <- suppressMessages(st_intersects(y.sf, check.sfc))
+    if (!(unique(vapply(tmp, length, 1)) == 1)) 
+      warning("Some of the validation data points are outside of the ", 
+              "provided plot limits", 
+              immediate. = TRUE)
+    rm(tmp, check.sfc, crop.bbox)
+  }
+   
+  ### Calculate metrics and plot, if necessary
+  metrics.list <- lapply(dates.both, function(i, j, j.plot, k) {
+    print(i)
+    k.curr <- k %>% filter(preds_col == i)
+    
+    # browser()
+    # If desired, plot and save predictions with overlaid validation data
+    if (!is.null(plot.path)) {
+      png(paste0(plot.path, i, ".png"), width = 6, height = 6*hw, 
+          units = 'in', res = 450)
+      
+      plot(j[i], axes = TRUE, border = NA, breaks = col.breaks, pal = col.pal, 
+           xaxt = "n", xlim = plot.xlim, ylim = plot.ylim, 
+           asp = 0, key.length = 1, key.pos = 4, reset = FALSE)
+      sf::.degAxis(1, at = c(-125, -120))
+      plot(st_geometry(filter(k.curr, valid_data_pa == 0)), add = TRUE, 
+           pch = 19, cex = 0.2, col = "black", 
+           key.pos = NULL, reset = FALSE)
+      plot(st_geometry(filter(k.curr, valid_data_pa == 1)), add = TRUE, 
+           pch = 19, cex = 0.3, col = "red", 
+           key.pos = NULL, reset = FALSE)
+      plot(map.base, add = TRUE, col = "tan", border = NA)
+      graphics::box()
+      legend("topright", title = "Validation data", legend = c("Pres", "Abs"), 
+             col = c("red", "black"), pch = 19, pt.cex = 1.5)
+      
+      dev.off()
+    }
+    
+    # Calculate metrics
+    if (length(unique(k.curr$valid_data_pa)) == 2) {
+      evaluation_metrics(j, i, k.curr, "valid_data_pa", count.flag = FALSE)
+    } else {
+      c(NA, NA, NA)
+    }
+  }, j = x.sf, j.plot = x.sf.crop, k = y.sf)
+  
+  metrics.df <- data.frame(
+    preds_col = dates.both, do.call(rbind, metrics.list), 
+    stringsAsFactors = FALSE
+  ) %>% 
+    select(preds_col, AUC = 2, TSS = 3)
+  
+  ### Write metrics to a csv file if desired
+  if (!is.null(csv.filename)) write_csv(metrics.df, path = csv.filename)
+  
+  ### Return data frame
+  metrics.df
+}
+
+
+# # Old code, stored here for now
+# j.bbox <- st_bbox(j)
+# xlim = c(j.bbox[c(1, 3)]), ylim = j.bbox[c(2, 4)], 
+# plot(k.curr["valid_data_pa"], add = TRUE, 
+#      pch = c(1, 19), cex = 0.4, pal = c("black", "red"), 
+#      key.pos = NULL, reset = FALSE)
+
+###############################################################################
---
+++ Whalepreds_evaluate_demo_rockfish.R
@@ -0,0 +1,141 @@
+### Demo script for using whalepreds_evaluate() to calculate
+###   evaluation metrics using different temporal aggreagations of predictions
+
+
+###############################################################################
+library(dplyr)
+library(eSDM)
+library(lubridate)
+library(purrr)
+library(readr)
+library(sf)
+
+
+source("Whalepreds_aggregate.R")
+source("Whalepreds_evaluate.R")
+
+
+# User identification
+# path.prefix: User-specifc; e.g., where Sam stores RDS files
+# filename.preds: Filename (including path) of csv file with whale prediction data
+# filename.validation: Filename (including path) of csv file with validation data
+# path.out: Path to folder where evaluation outputs (plots, csv files) will be saved
+#   This folder currently MUST contain 'monthly, '4day', and '7day' folders
+
+source("User_script_local.R")
+if (user == "EAB") {
+  
+} else if (user == "KAF") {
+  
+} else if (user == "SMW") {
+  path.prefix <- "../RAIMBOW/" # What Sam uses for RDS files
+  filename.preds <- "../RAIMBOW/Data/Humpback 3km models/Model1_PredictionGrids/WEAR3km_76_2005-01-01to2019-08-14_daily_dens.csv"
+  filename.validation <- "../RAIMBOW/Data/Humpback 3km models/Validation/RREAS_1995-2016/RFwhales2_prey.csv"
+  path.out <- "../RAIMBOW/Plots/Whalepreds_evaluate/"
+} else {
+  stop("User not recognized")
+}
+
+
+###############################################################################
+# File reading and prep
+
+### Read in bidaily predictions
+x.curr <- readr::read_csv(filename.preds) %>% 
+  select(pixel, mlat, mlon, mlon360, areakm, starts_with("76.dens."))
+
+# saveRDS(x, file = paste0(path.prefix, "RDATA_files/3km_daily.rds"))
+# x.curr <- readRDS(paste0(path.prefix, "RDATA_files/3km_daily.rds")) %>% 
+#   select(pixel, mlat, mlon, mlon360, areakm, starts_with("76.dens."))
+# ^ Sam's time-saver: save data frame as RDS file for faster reading
+
+### Predictions aggregated below
+
+### Read in validation data
+y <- read_csv(
+  filename.validation, 
+  col_types = cols(
+    .default = col_double(),
+    GIS_KEY = col_character(),
+    dt = col_date(format = "%m/%d/%Y"),
+    sightcat = col_character()
+  )
+)
+
+
+###############################################################################
+# Runtime: ~7s for every date interval that has both preds and 
+#   validation data, i.e. the ## in 'Number of dates: ##' print statement
+
+### Monthly (note different x.col.idx)
+x.mon <- whalepreds_aggregate( 
+  x = x.curr, x.cols = 6:ncol(x.curr), x.col.idx = 9:18,
+  aggr.level = "monthly", range.dates = NULL, se.calc = FALSE
+)
+metrics.df.mon <- whalepreds_evaluate(
+  x = x.mon, y = y, 
+  x.cols = NULL, x.col.idx = 13:22, y.cols = c("lon", "lat", "dt", "HUWH"), 
+  grid.rad = 0.027/2, 
+  csv.filename = paste0(path.out, "monthly/metrics_monthly.csv"), 
+  plot.path = paste0(path.out, "monthly/"), 
+  plot.xlim = c(-126, -117), plot.ylim = c(32, 42), 
+  col.breaks = c(0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.09), col.pal = NULL 
+)
+
+### 14 days; ~3.5 min
+x.14d <- whalepreds_aggregate(
+  x = x.curr, x.cols = 6:ncol(x.curr), x.col.idx = 9:18,
+  aggr.level = "14day", range.dates = NULL, se.calc = FALSE
+)
+metrics.df.14d <- whalepreds_evaluate(
+  x = x.14d, y = y, 
+  x.cols = NULL, x.col.idx = 11:20, y.cols = c("lon", "lat", "dt", "HUWH"), 
+  grid.rad = 0.027/2, 
+  csv.filename = paste0(path.out, "14day/metrics_14day.csv"), 
+  plot.path = paste0(path.out, "14day/"), 
+  plot.xlim = c(-126, -117), plot.ylim = c(32, 42), 
+  col.breaks = c(0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.09), col.pal = NULL 
+)
+
+### 7 days; ~5.5min
+x.7d <- whalepreds_aggregate( 
+  x = x.curr, x.cols = 6:ncol(x.curr), x.col.idx = 9:18,
+  aggr.level = "7day", range.dates = NULL, se.calc = FALSE
+)
+metrics.df.7d <- whalepreds_evaluate(
+  x = x.7d, y = y, 
+  x.cols = NULL, x.col.idx = 10:19, y.cols = c("lon", "lat", "dt", "HUWH"), 
+  grid.rad = 0.027/2, 
+  csv.filename = paste0(path.out, "7day/metrics_7day.csv"), 
+  plot.path = paste0(path.out, "7day/"), 
+  plot.xlim = c(-126, -117), plot.ylim = c(32, 42), 
+  col.breaks = c(0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.09), col.pal = NULL
+)
+
+
+###############################################################################
+# Examine histograms of AUC and TSS values
+
+### Read in data from csv files if necessary
+# metrics.df.mon <- read_csv("Plots/Whalepreds_evaluate/monthly/metrics_monthly.csv")
+# metrics.df.14d <- read_csv("Plots/Whalepreds_evaluate/14day/metrics_14day.csv")
+# metrics.df.7d  <- read_csv("Plots/Whalepreds_evaluate/7day/metrics_7day.csv")
+
+### Histograms of AUC values
+png(paste0(path.out, "Histogram_AUC.png"), width = 6, height = 6, units = "in", res = 450)
+opar <- par(mfrow = c(2, 2))
+hist(metrics.df.mon$AUC, breaks = seq(0, 1, by = 0.1))
+hist(metrics.df.14d$AUC, breaks = seq(0, 1, by = 0.1))
+hist(metrics.df.7d$AUC, breaks = seq(0, 1, by = 0.1))
+par(opar)
+dev.off()
+
+### Histogram of TSS values
+png(paste0(path.out, "Histogram_TSS.png"), width = 6, height = 6, units = "in", res = 450)
+opar <- par(mfrow = c(2, 2))
+hist(metrics.df.mon$TSS, breaks = seq(0, 1, by = 0.1))
+hist(metrics.df.14d$TSS, breaks = seq(0, 1, by = 0.1))
+hist(metrics.df.7d$TSS, breaks = seq(0, 1, by = 0.1))
+par(opar)
+dev.off()
+###############################################################################
