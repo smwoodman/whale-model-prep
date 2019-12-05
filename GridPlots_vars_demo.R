c5da2aeae30125bf08187f8e38f74f47d9d5b029
Sam Woodman <swoodman213@gmail.com>
2019-12-05 11:47:49 -0800

    Plot more input vars

--- GridPlots_vars_demo.R
+++ GridPlots_vars_demo.R
@@ -13,8 +13,8 @@ source('C:/SMW/RAIMBOW/whale-model-prep/GridPlots_vars.R')
 ### Practice
 d <- read.csv("../whale-model-prep_data/Grid/Grid_all/WEAR_3km_2005-01-11.csv")
 gridplots_vars(
-  "../whale-model-prep_data/Grid/Grid_all/WEAR_3km_2005-01-11.csv", 
-  plot.folder = NULL, "sst.mean"
+  "../whale-model-prep_data/Grid/Grid_all/WEAR_3km_2005-01-13.csv", 
+  plot.folder = NULL, "ild.mean"
 )
 
 ### Get file names
@@ -39,10 +39,9 @@ stopCluster(cl)
 # tmp.all.df <- NULL
 # for(j in files.curr) tmp.all.df <- rbind(tmp.all.df, read.csv(j)) #160s
 
-range(tmp.all$sst.mean, na.rm = TRUE)
-range(tmp.all$ssh.mean, na.rm = TRUE)
 
 ### Plot SST
+range(tmp.all.df$sst.mean, na.rm = TRUE)
 gridplots_vars(
   files.toplot = files.curr, 
   plot.folder = "../whale-model-prep_data/Grid/GridPlots_vars/SST/", 
@@ -51,11 +50,36 @@ gridplots_vars(
 )
 
 ### Plot SSH
+range(tmp.all.df$ssh.mean, na.rm = TRUE)
 gridplots_vars(
   files.toplot = files.curr, 
   plot.folder = "../whale-model-prep_data/Grid/GridPlots_vars/SSH/", 
   var.name = "ssh.mean", 
   col.breaks = c(-0.07, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6)
+)
+
+### Plot MLD (ILD)
+range(tmp.all.df$ild.mean, na.rm = TRUE)
+gridplots_vars(
+  files.toplot = files.curr, 
+  plot.folder = "../whale-model-prep_data/Grid/GridPlots_vars/ILD/", 
+  var.name = "ild.mean", 
+  col.breaks = c(0, 10, 20, 40, 60, 80, 100, 150, 212)
+)
+gridplots_vars(
+  files.toplot = files.curr, 
+  plot.folder = "../whale-model-prep_data/Grid/GridPlots_vars/ILD_v2/", 
+  var.name = "ild.mean", 
+  col.breaks = c(0, 4, 8, 12, 16, 20, 50, 150, 212)
+)
+
+### murSST
+range(tmp.all.df$analysed_sst.mean, na.rm = TRUE)
+gridplots_vars(
+  files.toplot = files.curr, 
+  plot.folder = "../whale-model-prep_data/Grid/GridPlots_vars/murSST/", 
+  var.name = "analysed_sst.mean", 
+  col.breaks = c(7, 10, 12, 14, 16, 18, 20, 22, 24)
 )
 
 ###############################################################################
